ae7cfe865d3981d026d6eb40cf09c504ca0108a9
Pichai Kosawanitchakit <pichai@pirsquare.net>
2022-04-16 17:23:38 +0700

    fix: change GA_TRACKING_ID

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = "G-7JJ1627QB3";
+export const GA_TRACKING_ID = "UA-118266267-2";
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL): void => {
